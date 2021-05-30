65dd5b1de6864fd1d4ed8a564d9fc1333252827d
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-05-30 09:27:23 -0700

    Added  div class for header below navigation bar

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-document.getElementById("myDiv").style.margin = "50px 10px 20px 30px";
+document.getElementById("myMargin").style.margin = "50px 10px 20px 30px";
+document.getElementById("myBorder").style.border = "50px 10px 20px 30px";
+
+
+
+
+
+
 
 /* Toggle between adding and removing tge "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
