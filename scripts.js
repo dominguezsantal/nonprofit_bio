a0725f4cd8b09472d9586d42dec8ca1d1f9f7d50
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-05-29 22:10:19 -0700

    Added margin and header inline box attributes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+document.getElementById("myDiv").style.margin = "50px 10px 20px 30px";
+
+/* Toggle between adding and removing tge "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,3 +9,4 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
