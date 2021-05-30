024d5d3341243e4bccbf81a40979a02ec00aafea
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-05-30 09:52:20 -0700

    Changed top Navigation class to navigation bar or navBar

--- scripts.js
+++ scripts.js
@@ -1,19 +1,13 @@
 document.getElementById("myMargin").style.margin = "50px 10px 20px 30px";
 document.getElementById("myBorder").style.border = "50px 10px 20px 30px";
 
-
-
-
-
-
-
 /* Toggle between adding and removing tge "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+function myNavBar() {
+  var x = document.getElementById("myNavBar");
+  if (x.className === "navBar") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "navBar";
   }
 }
 
